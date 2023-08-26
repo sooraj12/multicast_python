2fa46271ecb1c110d4f8bc673679dc16050a85cf
sooraj12 <soorajshaji33@gmail.com>
2023-08-26 17:44:19 +0530

    added sender file

---
+++ multi_cast_sender.py
@@ -0,0 +1,26 @@
+import socket
+import json
+
+hostip = '192.168.1.3'
+grpaddr = '239.1.2.3'
+port = 5001
+msg = {'message': 'message from sender'}
+
+
+sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM,
+                       proto=socket.IPPROTO_UDP, fileno=None)
+
+mcgrp = (grpaddr, port)
+
+sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF,
+                  socket.inet_aton(hostip))
+
+print(f'sending {msg}')
+encoded = json.dumps(msg).encode('utf-8')
+sent = sender.sendto(encoded, mcgrp)
+
+print('Waiting to receive')
+data, server = sender.recvfrom(16)
+print(f'received {data.decode()} from {server}')
+sender.close()
