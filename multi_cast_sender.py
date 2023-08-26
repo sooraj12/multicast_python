c64ad0632dd6c8edf8760d01af80b4aff51dd6c7
sooraj <soorajshaji33@gmail.com>
2023-08-26 19:11:19 +0530

    removed flask from sender file

--- multi_cast_sender.py
+++ multi_cast_sender.py
@@ -1,18 +1,15 @@
 import socket
 import json
-import asyncio
 
-from flask import Flask, jsonify
+
 
 hostip = '192.168.1.3'
 grpaddr = '239.1.2.3'
 port = 5001
 msg = {'message': 'message from sender'}
 
-app = Flask(__name__)
 
-
-async def send_and_receive():
+def send_and_receive():
     sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM,
                        proto=socket.IPPROTO_UDP, fileno=None)
     
@@ -25,22 +22,13 @@ async def send_and_receive():
     print(f'sending {msg}')
     encoded = json.dumps(msg).encode('utf-8')
     sent = sender.sendto(encoded, mcgrp)
-
-    loop = asyncio.get_event_loop()
-    receiver_task = loop.run_in_executor(None, sender.recvfrom, 16)
-    await asyncio.wait([receiver_task])
-
-    data, server = receiver_task.result()
+   
+    data, server =  sender.recvfrom(16)
     print(f'received {data.decode()} from {server}')
     sender.close()
 
-@app.route('/health', methods=["GET"])
-def health():
-    print('test request received')
-    return jsonify(message="OK"), 200
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    send_receive_task = loop.create_task(send_and_receive())
+    send_and_receive()
 
-    app.run(host='0.0.0.0', port=5000, threaded=True)
