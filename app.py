3d6517b350457de4654529826ee9a78063dfe311
sooraj <soorajshaji33@gmail.com>
2023-08-27 12:43:21 +0530

    made production ready

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 import socket
 import json
 import struct
-from flask import Flask, jsonify
 import threading
 import signal
 import atexit
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from flask import Flask, jsonify
 
-# Global variables
-shutdown_event = threading.Event()
+app = Flask(__name__)
+
+# Configuration
 hostip = '192.168.1.3'
 grpaddr = '239.1.2.3'
 port = 5001
+max_workers = 10  # Number of threads in the thread pool
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Global variables
+shutdown_event = threading.Event()
+channel = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP, fileno=None)
 msg = {'type': 'message', 'message': 'message from mac', 'status': 'success'}
 ack = {'type': 'ack', 'res': 'acknowledge from mac'}
 
-channel = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP, fileno=None)
-channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(hostip))
-channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
-bindaddr = ('', port)
-channel.bind(bindaddr)
-mreq = struct.pack("=4s4s", socket.inet_aton(grpaddr), socket.inet_aton(hostip))
-channel.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+try:
+    channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+    channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(hostip))
+    channel.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
+    bindaddr = ('', port)
+    channel.bind(bindaddr)
+    mreq = struct.pack("=4s4s", socket.inet_aton(grpaddr), socket.inet_aton(hostip))
+    channel.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+except Exception as e:
+    logger.error(f"Error setting up socket: {e}")
+    raise SystemExit(1)
+
+# Thread pool for handling requests
+thread_pool = ThreadPoolExecutor(max_workers=max_workers)
+
 
 def send_and_receive():
     try:
@@ -31,19 +50,17 @@ def send_and_receive():
         channel.sendto(encoded, mcgrp)
 
     except Exception as e:
-        print(f"Error in sending: {e}")
+        logger.error(f"Error in sending: {e}")
 
 def receive_messages():
     try:
         while not shutdown_event.is_set():
             buf, senderaddr = channel.recvfrom(1024)
-
-            msg = json.loads(buf)
-
-            print(f"Received message from {senderaddr}: {msg}")
+            received_msg = json.loads(buf)
+            logger.info(f"Received message from {senderaddr}: {received_msg}")
 
     except Exception as e:
-        print(f"Error in receiving: {e}")
+        logger.error(f"Error in receiving: {e}")
 
 def shutdown():
     global shutdown_event
@@ -58,9 +75,7 @@ def health():
 
 @app.route('/send', methods=['GET'])
 def send():
-    send_thread = threading.Thread(target=send_and_receive)
-    send_thread.daemon = True
-    send_thread.start()
+    thread_pool.submit(send_and_receive)
     return jsonify(message="OK"), 200
 
 if __name__ == "__main__":
